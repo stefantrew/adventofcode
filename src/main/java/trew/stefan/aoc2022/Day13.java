53e792a6f535a99e2f15a6b3e9d9e27e137a09b2
Stefan Trew <stefant@fsi.zone>
2022-12-14 06:59:19 +0200

    add day 13 p2:

--- src/main/java/trew/stefan/aoc2022/Day13.java
+++ src/main/java/trew/stefan/aoc2022/Day13.java
@@ -20,7 +20,6 @@ public class Day13 extends AbstractAOC {
         var list = getStringInput("_sample");
 
 
-
         var ascore = 0;
         var queue = new LinkedList<>(list);
         var parser = new JSONParser();
@@ -30,12 +29,10 @@ public class Day13 extends AbstractAOC {
             try {
                 JSONArray left = (JSONArray) parser.parse(queue.pop());
                 JSONArray right = (JSONArray) parser.parse(queue.pop());
-                log.info("==================================================");
                 var result = compare(left, right, "  ");
                 if (Boolean.TRUE.equals(result)) {
                     ascore += count + 1;
                 }
-                log.info("{} result {}", count + 1, result);
                 count++;
             } catch (ParseException e) {
                 throw new RuntimeException(e);
@@ -46,9 +43,6 @@ public class Day13 extends AbstractAOC {
 
                 queue.pop();
             }
-//            if (count == 4)
-//            break;
-
         }
 
 
@@ -57,10 +51,10 @@ public class Day13 extends AbstractAOC {
 
     private Boolean compare(JSONArray left, JSONArray right, String indent) {
 
-        log.info("{}Comparing {} vs {}", indent, left.toJSONString(), right.toJSONString());
+//        log.info("{}Comparing {} vs {}", indent, left.toJSONString(), right.toJSONString());
         for (int i = 0; i < left.size(); i++) {
             if (i > right.size() - 1) {
-                log.info("Right ran out of items");
+//                log.info("Right ran out of items");
                 return false;
             }
 
@@ -69,12 +63,12 @@ public class Day13 extends AbstractAOC {
 
             switch (leftEle) {
                 case Long l && rightEle instanceof Long r -> {
-                    log.info("{}Comparing {} vs {}", indent, l, r);
+//                    log.info("{}Comparing {} vs {}", indent, l, r);
                     if (l < r) {
-                        log.info("{}Left side is smaller", indent);
+//                        log.info("{}Left side is smaller", indent);
                         return true;
                     } else if (r < l) {
-                        log.info("{}Right side is smaller, so inputs are not in the right order", indent);
+//                        log.info("{}Right side is smaller, so inputs are not in the right order", indent);
                         return false;
                     }
                 }
@@ -106,7 +100,7 @@ public class Day13 extends AbstractAOC {
         }
         if (left.size() != right.size()) {
 
-            log.info("{}Left side ran out of items, so inputs are in the right order", indent);
+//            log.info("{}Left side ran out of items, so inputs are in the right order", indent);
             return true;
         }
 
@@ -117,11 +111,35 @@ public class Day13 extends AbstractAOC {
     @Override
     public String runPart2() {
         var list = getStringInput("");
+        var parser = new JSONParser();
+        list.removeIf(String::isEmpty);
+        log.info("{}", list);
+        var e1 = "[[2]]";
+        list.add(e1);
+        var e2 = "[[6]]";
+        list.add(e2);
+        list.sort((o1, o2) -> {
 
+            try {
+                JSONArray left = (JSONArray) parser.parse(o1);
+                JSONArray right = (JSONArray) parser.parse(o2);
+                if (compare(left, right, "")) {
+                    return -1;
+                }
+                return 1;
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        for (String s : list) {
+
+            log.info("{}", s);
+        }
+        var index1 = list.indexOf(e1) + 1;
+        var index2 = list.indexOf(e2) + 1;
+        log.info("{} {}", index1, index2);
         var ascore = 0;
-
-
-        return String.valueOf(ascore);
+        return String.valueOf(index1 * index2);
     }
 
     @Override
