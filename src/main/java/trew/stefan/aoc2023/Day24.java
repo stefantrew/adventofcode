17e93a6595cfa6ffb03a84c6cd8cc8c413e7122c
Stefan Trew <stefan.trew@gmail.com>
2025-07-22 10:36:08 +0200

    Added day 24 part 1

--- src/main/java/trew/stefan/aoc2023/Day24.java
+++ src/main/java/trew/stefan/aoc2023/Day24.java
@@ -4,6 +4,8 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import trew.stefan.AbstractAOC;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
@@ -16,8 +18,8 @@ public class Day24 extends AbstractAOC {
         long x;
         long y;
         long z;
-        double c;
-        double m;
+        BigDecimal c;
+        BigDecimal m;
         long dx;
         long dy;
         long dz;
@@ -29,19 +31,15 @@ public class Day24 extends AbstractAOC {
             this.dx = dx;
             this.dy = dy;
             this.dz = dz;
-            m = (double) dy / dx;
-            this.c = (y - m * x);
+            m = BigDecimal.valueOf(dy).divide(BigDecimal.valueOf(dx), 10, RoundingMode.FLOOR);
+            this.c = BigDecimal.valueOf(y).subtract(m.multiply(BigDecimal.valueOf(x)));
 
         }
 
-        public double getXIntercept(HailStone other) {
-            var temp = other.c - c;
-            var temp2 = other.m - m;
-            return -(double) temp / temp2;
-        }
-
-        public double getYIntercept(HailStone other) {
-            return m * getXIntercept(other) + c;
+        public BigDecimal getXIntercept(HailStone other) {
+            var temp = other.c.subtract(c);
+            var temp2 = other.m.subtract(m);
+            return temp.divide(temp2, 10, RoundingMode.FLOOR).negate();
         }
 
 
@@ -51,35 +49,38 @@ public class Day24 extends AbstractAOC {
         log.info("==========================================================");
 
 
-        if (stone1.m == stone2.m) {
+        if (stone1.m.equals(stone2.m)) {
             log.info("Parallel lines");
             return false;
         }
 
-        var temp = stone1.c - stone2.c;
-        var temp2 = stone1.m - stone2.m;
-        var x = -(double) temp / temp2;
-        var y = stone1.m * x + stone1.c;
+        var temp = stone1.c.subtract(stone2.c);
+        var temp2 = stone1.m.subtract(stone2.m);
+        var x = temp.divide(temp2, 10, RoundingMode.FLOOR).negate();
+
+
+        var y = stone1.m.multiply(x).add(stone1.c);
 
         log.info("X = {}, y = {}", x, y);
 
 
-        if (x < min || x > max) {
+        if (x.longValue() < min || x.longValue() > max) {
             log.info("X out of bounds");
             return false;
         }
 
-        if (y < min || y > max) {
+        if (y.longValue() < min || y.longValue() > max) {
             log.info("Y out of bounds");
             return false;
         }
 
-        if (x > stone1.x && stone1.m < 0) {
+        if (x.subtract(BigDecimal.valueOf(stone1.x)).signum() * Math.signum(stone1.dx) < 0) {
             log.info("In the past for stone 1");
             return false;
         }
 
-        if (x > stone2.x && stone2.m < 0) {
+        if (x.subtract(BigDecimal.valueOf(stone2.x)).signum() * Math.signum(stone2.dx) < 0) {
+
             log.info("In the past for stone 2");
             return false;
         }
@@ -92,18 +93,13 @@ public class Day24 extends AbstractAOC {
     public String runPart1() {
 
         var total = 0;
-        var result = "";
 
         final String regex = "(\\d*), (\\d*), (\\d*) @ (-?\\d*), (-?\\d*), (-?\\d*)";
 
         var pattern = Pattern.compile(regex);
 
-//        var list = getStringInput().stream().map(this::mapper).toList();
         var stones = new ArrayList<HailStone>();
         var list = getStringInput("");
-//        var list = getLongInput();
-//        var list = getIntegerInput();
-//        var list = getDoubleInput();
 
         for (var s : list) {
             var matcher = pattern.matcher(s);
@@ -123,33 +119,9 @@ public class Day24 extends AbstractAOC {
 
             }
         }
-        //12807
         long min = 200000000000000L;
         long max = 400000000000000L;
-//        long min = 7;
-//        long max = 27;
-//
-//        var s1 = new HailStone(19, 13, 30, -2, 1, -2);
-//        var s2 = new HailStone(18, 19, 22, -1, -1, -2);
-//        log.info("inside: {} ", this.doesIntercept(s1, s2, min, max));
-//
-//        s1 = new HailStone(19, 13, 30, -2, 1, -2);
-//        s2 = new HailStone(20, 25, 34, -2, -2, -4);
-//        log.info("outside: {} ", this.doesIntercept(s1, s2, min, max));
-//
-//        s1 = new HailStone(19, 13, 30, -2, 1, -2);
-//        s2 = new HailStone(12, 31, 28, -1, -2, -1);
-//        log.info("outside: {} ", this.doesIntercept(s1, s2, min, max));
-//
-//        s1 = new HailStone(18, 19, 22, -1, -1, -2);
-//        s2 = new HailStone(20, 25, 34, -2, -2, -4);
-////        log.info("X 14.333 {}", s1.getXIntercept(s2));
-////        log.info("Y 15.333 {}", s1.getYIntercept(s2));
-//        log.info("parallel: {} ", this.doesIntercept(s1, s2, min, max));
-//
-//        s1 = new HailStone(19, 13, 30 , -2, 1, -2);
-//        s2 = new HailStone(20, 19, 15 , 1, -5, -3);
-//        log.info("in the past: {} ", this.doesIntercept(s1, s2, min, max));
+
 
         for (int i = 0; i < stones.size(); i++) {
             for (int j = i + 1; j < stones.size(); j++) {
@@ -168,15 +140,64 @@ public class Day24 extends AbstractAOC {
     @Override
     public String runPart2() {
 
+        return formatResult("");
+    }
 
-        var list = getStringInput();
+    public String testPart1() {
+
+        long min = 7;
+        long max = 27;
+
+        var s1 = new HailStone(19, 13, 30, -2, 1, -2);
+        var s2 = new HailStone(18, 19, 22, -1, -1, -2);
+        log.info("inside: {} ", this.doesIntercept(s1, s2, min, max));
+//
+        s1 = new HailStone(19, 13, 30, -2, 1, -2);
+        s2 = new HailStone(20, 25, 34, -2, -2, -4);
+        log.info("inside: {} ", this.doesIntercept(s1, s2, min, max));
+//
+        s1 = new HailStone(19, 13, 30, -2, 1, -2);
+        s2 = new HailStone(12, 31, 28, -1, -2, -1);
+        log.info("outside: {} ", this.doesIntercept(s1, s2, min, max));
+//
+        s1 = new HailStone(19, 13, 30, -2, 1, -2);
+        s2 = new HailStone(20, 19, 15, 1, -5, -3);
+        log.info("outside: {} ", this.doesIntercept(s1, s2, min, max));
+//
+
+        s1 = new HailStone(18, 19, 22, -1, -1, -2);
+        s2 = new HailStone(20, 25, 34, -2, -2, -4);
+        log.info("parallel: {} ", this.doesIntercept(s1, s2, min, max));
+
+
+        s1 = new HailStone(18, 19, 22, -1, -1, -2);
+        s2 = new HailStone(12, 31, 28, -1, -2, -1);
+        log.info("outside: {} ", this.doesIntercept(s1, s2, min, max));
+
+        s1 = new HailStone(18, 19, 22, -1, -1, -2);
+        s2 = new HailStone(20, 19, 15, 1, -5, -3);
+        log.info("past: {} ", this.doesIntercept(s1, s2, min, max));
+
+
+        s1 = new HailStone(20, 25, 34, -2, -2, -4);
+        s2 = new HailStone(12, 31, 28, -1, -2, -1);
+        log.info("past: {} ", this.doesIntercept(s1, s2, min, max));
+
+        s1 = new HailStone(20, 25, 34, -2, -2, -4);
+        s2 = new HailStone(20, 19, 15, 1, -5, -3);
+        log.info("past: {} ", this.doesIntercept(s1, s2, min, max));
+
+
+//        var s1 = new HailStone(12, 31, 28, -1, -2, -1);
+//        var s2 = new HailStone(20, 19, 15, 1, -5, -3);
+//        log.info("in the past: {} ", this.doesIntercept(s1, s2, min, max));
 
         return formatResult("");
     }
 
     @Override
     public String getAnswerPart1() {
-        return "";
+        return "16018";
     }
 
     @Override
